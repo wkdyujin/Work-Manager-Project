369be2d1e8a34cd9f286f7e6d4141728d4d8a465
wkdyujin <yujin1203713@gmail.com>
2024-03-03 17:07:52 +0900

    [Feat] Create UserList View

--- src/main/java/com/fisa/workmanager/controller/UserController.java
+++ src/main/java/com/fisa/workmanager/controller/UserController.java
@@ -3,6 +3,7 @@ package com.fisa.workmanager.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +22,11 @@ public class UserController {
 		this.userService = userService;
 	}
 	
-	@GetMapping("/")
-	public List<Employee> getUserList() {
+	@GetMapping("/list")
+	public String getUserList(Model model) {
 		List<Employee> userList = userService.getUserList();
-		return userList;
+		model.addAttribute("userList", userList);
+		return "user/list";
 	}
 	
 	@GetMapping("/{id}")
--- src/main/webapp/WEB-INF/views/include/header.jsp
+++ src/main/webapp/WEB-INF/views/include/header.jsp
@@ -34,7 +34,7 @@
 		                    EMPLOYEE
 		                </a>
 		                <div class="dropdown-menu" aria-labelledby="navbarDropdownHR">
-		                  <a class="dropdown-item" href="/employees">전체 직원 조회</a>
+		                  <a class="dropdown-item" href="/user/list">전체 직원 조회</a>
 		                  <a class="dropdown-item" href="/auth/register">직원 추가</a>
 		                </div>
 		            </li>
