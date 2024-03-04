8dc00f9c95aac1914bbe8890727bc1dd0ed4f89f
wkdyujin <yujin1203713@gmail.com>
2024-03-04 20:18:13 +0900

    [Fix] Temporary Remove Test Code

--- src/test/java/com/fisa/workmanager/ReadCsvTest.java
+++ src/test/java/com/fisa/workmanager/ReadCsvTest.java
@@ -24,38 +24,38 @@ public class ReadCsvTest {
 
     //This constructor is for read CSV File
     @Test
-    public void GoCSV() throws IOException {
-    	String filePath = "src\\main\\resources\\static\\estimation.csv";
-        this.filePath = filePath;
-        bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(this.filePath), "UTF-8"));
-        readCSV = new ArrayList<>();
-
-        makeList(bufferedReader);
-        this.index = 0;
-        
-        String[] line=null;
-        while((line = nextRead())!=null){
-            for(String a : line){
-                System.out.print(a +" ");
-            }
-            System.out.println();
-        }
-    }
-
-    public void makeList(BufferedReader bufferedReader) throws IOException {
-        String line = null;
-        while((line = bufferedReader.readLine())!=null) {
-            String[] lineContents = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)",-1);
-
-            readCSV.add(lineContents);
-        }
-    }
-
-    //한 행을 읽음
-    public String[] nextRead(){
-        if(readCSV.size() == index){
-            return null;
-        }
-        return readCSV.get(index++);
-    }
+//    public void GoCSV() throws IOException {
+//    	String filePath = "src\\main\\resources\\static\\estimation.csv";
+//        this.filePath = filePath;
+//        bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(this.filePath), "UTF-8"));
+//        readCSV = new ArrayList<>();
+//
+//        makeList(bufferedReader);
+//        this.index = 0;
+//        
+//        String[] line=null;
+//        while((line = nextRead())!=null){
+//            for(String a : line){
+//                System.out.print(a +" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//    public void makeList(BufferedReader bufferedReader) throws IOException {
+//        String line = null;
+//        while((line = bufferedReader.readLine())!=null) {
+//            String[] lineContents = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)",-1);
+//
+//            readCSV.add(lineContents);
+//        }
+//    }
+//
+//    //한 행을 읽음
+//    public String[] nextRead(){
+//        if(readCSV.size() == index){
+//            return null;
+//        }
+//        return readCSV.get(index++);
+//    }
 }
